8937ce935a81398d0fe8b60d75e1e46b77df6fb9
Sufal Roy <sufalroy1997@gmail.com>
2024-03-04 16:10:09 +0530

    refactor method authority in userService

--- simple-jwt-be/src/main/java/org/sample/simplejwtbe/user/services/UserService.java
+++ simple-jwt-be/src/main/java/org/sample/simplejwtbe/user/services/UserService.java
@@ -1,6 +1,6 @@
 package org.sample.simplejwtbe.user.services;
 
-import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
@@ -10,7 +10,7 @@ import java.util.Map;
 @Service
 public class UserService {
 
-    @PostAuthorize("hasAuthority('SCOPE_read')")
+    @PreAuthorize("hasAuthority('SCOPE_read')")
     public Map<String, String> greet() {
         var jwt = (Jwt) SecurityContextHolder.getContext()
                 .getAuthentication()
