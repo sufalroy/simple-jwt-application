3206c4e4f6caf1da9d303a5eafccdb646170d753
Sufal Roy <sufalroy1997@gmail.com>
2024-03-04 15:31:27 +0530

    Add method authority

--- simple-jwt-be/src/main/java/org/sample/simplejwtbe/SimpleJwtBeApplication.java
+++ simple-jwt-be/src/main/java/org/sample/simplejwtbe/SimpleJwtBeApplication.java
@@ -2,8 +2,10 @@ package org.sample.simplejwtbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
 public class SimpleJwtBeApplication {
 
     public static void main(String[] args) {
--- simple-jwt-be/src/main/java/org/sample/simplejwtbe/user/services/UserService.java
+++ simple-jwt-be/src/main/java/org/sample/simplejwtbe/user/services/UserService.java
@@ -1,5 +1,6 @@
 package org.sample.simplejwtbe.user.services;
 
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import java.util.Map;
 @Service
 public class UserService {
 
+    @PostAuthorize("hasAuthority('SCOPE_read')")
     public Map<String, String> greet() {
         var jwt = (Jwt) SecurityContextHolder.getContext()
                 .getAuthentication()
